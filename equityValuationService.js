de87a86783cda955fdeb5e0a57b5fa8e01db0c04
sysadmin <sysadmin@Administrator.home>
2020-11-15 16:51:08 -0500

    created class valuationService

--- equityValuationService.js
+++ equityValuationService.js
@@ -1,37 +1,36 @@
 const lib = require('./utilities.js') ;
+const valuationService = require('./valuationService.js') ;
 const { dd } = require('./data_dictionary.js') ;
 
-module.exports = class equityValuationService
+module.exports = class equityValuationService extends valuationService
 {
 
-    static value(vObj, vContext, marketDataServer)
-    {
-        let priceObj = marketDataServer.priceObjFor(vObj) ;
-        let tv = -99999 ;
-        
-        
-        let price = -1 ;
-        let price_time = "1/1/1900";
-
-        
-        if (priceObj != undefined)
-        {
-            price_time = priceObj.get(dd.PRICE_TIME);
-            price = priceObj.get(dd.PRICE) ;
-        }
-
-        vObj.addItem(dd.PRICE, price) ;
-        vObj.addItem(dd.PRICE_TIME, price_time) ;
-
-        
-        let position = vObj.get(dd.SIZE)  ;
-        
-        if (price >0)
-            tv = position * price ;
-        
-        vObj.addItem(dd.TV, tv) ;
-        
-//        console.log(vObj) ;
-                
-    }
+//    static value(vObj, vContext, marketDataServer)
+//    {
+//        let priceObj = marketDataServer.priceObjFor(vObj) ;
+//        let tv = -99999 ;
+//        
+//        
+//        let price = -1 ;
+//        let price_time = "1/1/1900";
+//
+//        
+//        if (priceObj != undefined)
+//        {
+//            price_time = priceObj.get(dd.PRICE_TIME);
+//            price = priceObj.get(dd.PRICE) ;
+//        }
+//
+//        vObj.addItem(dd.PRICE, price) ;
+//        vObj.addItem(dd.PRICE_TIME, price_time) ;
+//
+//        
+//        let position = vObj.get(dd.SIZE)  ;
+//        
+//        if (price >0)
+//            tv = position * price ;
+//        
+//        vObj.addItem(dd.TV, tv) ;
+//                        
+//    }
 }
---
+++ valuationService.js
@@ -0,0 +1,35 @@
+const lib = require('./utilities.js') ;
+const { dd } = require('./data_dictionary.js') ;
+
+module.exports = class valuationService
+{
+
+    static value(vObj, vContext, marketDataServer)
+    {
+        let priceObj = marketDataServer.priceObjFor(vObj) ;
+        let tv = -99999 ;
+        
+        
+        let price = -1 ;
+        let price_time = "1/1/1900";
+
+        
+        if (priceObj != undefined)
+        {
+            price_time = priceObj.get(dd.PRICE_TIME);
+            price = priceObj.get(dd.PRICE) ;
+        }
+
+        vObj.addItem(dd.PRICE, price) ;
+        vObj.addItem(dd.PRICE_TIME, price_time) ;
+
+        
+        let position = vObj.get(dd.SIZE)  ;
+        
+        if (price >0)
+            tv = position * price ;
+        
+        vObj.addItem(dd.TV, tv) ;
+                        
+    }
+}
